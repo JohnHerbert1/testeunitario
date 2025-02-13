c9070e5487bd07003269b883aeabdcbf54ae6686
JohnHerbert1 <johnherbertfre98@gmail.com>
2025-02-13 19:50:27 -0300

    Teste

---
+++ src/main/java/testeunitario/TesteUnitarioApplication.java
@@ -0,0 +1,13 @@
+package testeunitario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteUnitarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteUnitarioApplication.class, args);
+	}
+
+}
